70ffc8192aa76a80fbb80789d76f6093d16d9fd0
kristofermc <kris.jussi@gmail.com>
2020-10-02 19:14:43 +0300

    Simple React Native Application for studies

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as secondapp} from './app.json';
+
+AppRegistry.registerComponent('secondapp', () => App);
